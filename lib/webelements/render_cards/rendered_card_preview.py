0468938f72f08fc09aad84a957efac1a464e7aaf
PatrickM <saluk64007@gmail.com>
2025-11-26 12:09:25 -0800

    Move zoom of rendered card preview to adjust the zoom of all outputs at once

--- lib/webelements/render_cards/rendered_card_preview.py
+++ lib/webelements/render_cards/rendered_card_preview.py
@@ -10,6 +10,8 @@ from lib.webelements.code_editor import CodeEditor
 class RenderedCardPreview:
     def __init__(self, view):
         self.view = view
+        self.zoom_levels = range(200, 1200, 200)
+        self.zoom = self.zoom_levels[0]
 
     @property
     def project(self):
@@ -32,48 +34,62 @@ class RenderedCardPreview:
         if not self.project:
             ui.label("No project")
             return
-        with ui.row().classes("w-full"):
-            with ui.grid(columns="200px auto"):
-                with ui.card().classes("w-[500px]").tight() as dragable_card:
-                    with ui.expansion("Viewed Outputs").classes("font-size-8 padding-0"):
-                        checks = []
-                        with ui.button_group():
-                            def f_all():
-                                for k,c in checks:
-                                    c.set_value(True)
-                            ui.button("All", on_click=f_all)
-                            def f_none():
-                                for k,c in checks:
-                                    c.set_value(False)
-                            ui.button("None", on_click=f_none)
-                        for output_key in self.project.outputs.keys():
-                            output = self.project.outputs[output_key]
-                            c = ui.checkbox(output.file_name, value=output_key in self.project.viewed_output)
-                            checks.append((output_key,c))
-                        def set_check():
-                            viewed_output = []
-                            for output_key, check in checks:
-                                if check.value:
-                                    viewed_output.append(output_key)
-                            self.project.viewed_output = viewed_output
-                            self.project.save()
-                            self.render_selected_project_outputs.refresh()
-                        [c[1].on_value_change(set_check) for c in checks]
-                    async def refresh_all():
-                        global_cache.clear()
-                        await self.project.dirty_outputs()
-                        await self.project.load_data()
-                        self.render_selected_project_outputs.refresh()
-                        self.view.refresh_project()
-                    ui.button("Refresh all").on_click(refresh_all)
-                    with ui.scroll_area().classes("w-full") as draggable_scroll:
-                        self.dragscroll(dragable_card, draggable_scroll, 3)
-                        print("about to render selected project outputs")
-                        await self.render_selected_project_outputs()
+        with ui.grid().classes("w-full") as dragable_card:
+            with ui.expansion("Viewed Outputs").classes("font-size-8 padding-0"):
+                checks = []
+                with ui.button_group():
+                    def f_all():
+                        for k,c in checks:
+                            c.set_value(True)
+                    ui.button("All", on_click=f_all)
+                    def f_none():
+                        for k,c in checks:
+                            c.set_value(False)
+                    ui.button("None", on_click=f_none)
+                for output_key in self.project.outputs.keys():
+                    output = self.project.outputs[output_key]
+                    c = ui.checkbox(output.file_name, value=output_key in self.project.viewed_output)
+                    checks.append((output_key,c))
+                def set_check():
+                    viewed_output = []
+                    for output_key, check in checks:
+                        if check.value:
+                            viewed_output.append(output_key)
+                    self.project.viewed_output = viewed_output
+                    self.project.save()
+                    self.render_selected_project_outputs.refresh()
+                [c[1].on_value_change(set_check) for c in checks]
+            async def refresh_all():
+                global_cache.clear()
+                await self.project.dirty_outputs()
+                await self.project.load_data()
+                self.render_selected_project_outputs.refresh()
+                self.view.refresh_project()
+            with ui.row():
+                ui.button("Refresh all").on_click(refresh_all)
+                await self.zoom_buttons()
+            await self.render_selected_project_outputs()
+
+    async def zoom_buttons(self):
+        def set_zoom(adj):
+            i = self.zoom_levels.index(self.zoom)
+            i += adj
+            if i<0 or i>=len(self.zoom_levels):
+                return
+            self.zoom = self.zoom_levels[i]
+            for im_el in self.image_element_list:
+                im_el.classes(replace=f"w-[{int(self.zoom)}px]")
+                im_el.update()
+        with ui.button_group():
+            zin:ui.button = ui.button("+")
+            zin.tailwind.font_size('xs')
+            zout:ui.button = ui.button("-")
+            zout.tailwind.font_size('xs')
+        zin.on_click(lambda:set_zoom(1))
+        zout.on_click(lambda:set_zoom(-1))
 
     @ui.refreshable
     async def render_selected_project_outputs(self):
-        zoom_levels = range(200, 1200, 200)
         print("render selected")
         if self.view.progress:
             try:
@@ -86,38 +102,22 @@ class RenderedCardPreview:
             type="ongoing",
             spinner=True
         )
-        # TODO mselfe this to the output view class
         self.image_element_list = []
         self.output_list = []
-        for output_key in self.project.viewed_output:
-            output:Output = self.project.outputs[output_key]
-            with ui.card():
-                with ui.button_group():
-                    zin:ui.button = ui.button("+")
-                    zin.tailwind.font_size('xs')
-                    zout:ui.button = ui.button("-")
-                    zout.tailwind.font_size('xs')
-                    ui.label(output.file_name).classes("m-3 font-bold")
-                    ui.label(output.data_source_name).classes("m-3 font-italic")
-                    for t in await output.templates_used(self.project):
-                        if not t:
-                            ui.notification(f'Error: output file {output.file_name} is missing template')
-                        ui.label("["+t+"]").classes("m-3")
-                zoom = zoom_levels[len(zoom_levels)//2]
-                im_el = ui.image("").classes(f'w-[{zoom}px]').style("cursor: zoom-in;")
-                im_el.zoom = zoom
-                def set_zoom(adj, im_el=im_el):
-                    i = zoom_levels.index(im_el.zoom)
-                    i += adj
-                    if i<0 or i>=len(zoom_levels):
-                        return
-                    im_el.zoom = zoom_levels[i]
-                    im_el.classes(replace=f"w-[{int(im_el.zoom)}px]")
-                    im_el.update()
-                zin.on_click(lambda im_el=im_el:set_zoom(1, im_el))
-                zout.on_click(lambda im_el=im_el:set_zoom(-1, im_el))
-                self.image_element_list.append(im_el)
-                self.output_list.append(output)
+        with ui.grid(columns=2).classes('w-fit'):
+            for output_key in self.project.viewed_output:
+                output:Output = self.project.outputs[output_key]
+                with ui.card().tight().classes("w-fit"):
+                    with ui.row():
+                        ui.label(output.file_name).classes("m-3 font-bold")
+                        ui.label(output.data_source_name).classes("m-3 font-italic")
+                        for t in await output.templates_used(self.project):
+                            if not t:
+                                ui.notification(f'Error: output file {output.file_name} is missing template')
+                            ui.label("["+t+"]").classes("m-3")
+                    im_el = ui.image("").classes(f'w-[{self.zoom}px]')
+                    self.image_element_list.append(im_el)
+                    self.output_list.append(output)
         ui.timer(0.1, lambda:self.render_images(self.image_element_list, self.output_list), once=True)
 
     def dragscroll(self, el, scroll_area:ui.scroll_area, speed=1):
